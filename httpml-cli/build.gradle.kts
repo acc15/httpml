10e7f9f953c628c4f7d0d058d5dad7419b38e8ee
Vyacheslav Mayorov <vyacheslav.mayorov@gmail.com>
2021-11-24 02:17:19 +0300

    init commit

---
+++ httpml-cli/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.6.0"
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+}
