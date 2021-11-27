98913c7ecc5c33f690d35370b7b5b90928fb0a32
Vyacheslav Mayorov <vyacheslav.mayorov@gmail.com>
2021-11-27 21:02:05 +0300

    javacc config

--- httpml-expression/build.gradle.kts
+++ httpml-expression/build.gradle.kts
@@ -1,4 +1,16 @@
 plugins {
     id("httpml.kotlin-conventions")
     `java-library`
+    id("com.intershop.gradle.javacc") version "4.0.1"
+}
+
+javacc {
+    // configuration container for all javacc configurations
+    configs {
+        register("template") {
+            inputFile = file("src/main/javacc/HttpmlExpression.jj")
+            packageName = "httpml.expression.parser"
+            lookahead = 2
+        }
+    }
 }
